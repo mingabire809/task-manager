8f2442a00265408086d328585fdef1953953dd9e
mingabire809 <michelingabire190508@daystar.ac.ke>
2022-07-31 15:09:46 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks')
+const connectDB = require('../taskmanager/db/connect')
+require('dotenv').config()
+const notFound = require('./middleware/not-found')
+const errorHandleMiddleware = require('./middleware/error-handler')
+
+
+//middleware
+app.use(express.static('./public'))
+app.use(express.json())
+
+
+//routes
+app.use('/api/v1/tasks', tasks)
+
+app.use(notFound)
+app.use(errorHandleMiddleware)
+
+const port  = process.env.PORT || 3000
+
+const start = async ()=>{
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`server is listening on port ${port}`))
+    } catch(error){
+        console.log(error)
+    }
+}
+
+start()
